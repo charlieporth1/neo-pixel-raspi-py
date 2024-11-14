8bec8b7c3e1ef10daac115d085cd48f79d7acc0b
Charlie Porth <charlieporth1@gmail.com>
2024-11-14 15:01:14 -0600

    Xtra scripts

--- helper/scripts/add-ppa.sh
+++ helper/scripts/add-ppa.sh
@@ -2,6 +2,7 @@
 
 sudo apt -y update
 sudo apt install -y python3-apt
+sudo apt install -y software-properties-common
 
 echo -e '\n' | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt -y update
