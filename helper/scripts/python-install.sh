cb2b2dfa453a04e81711e4dba78b146cef0f1068
Charlie Porth <charlieporth1@gmail.com>
2024-11-14 15:38:25 -0600

    Updated py ins

--- helper/scripts/python-install.sh
+++ helper/scripts/python-install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+ver=3.7
+
 sudo apt -y update
+
 sudo apt install -y python3.7
 sudo apt install -y python3.7-dev
 sudo apt install -y python3.7-distutils
 sudo apt install -y python3.7-minimal
+sudo apt install -y python3.7-full
 
 sudo apt install -y libpython3.7
 sudo apt install -y libpython3.7-dev
