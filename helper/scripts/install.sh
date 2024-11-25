5d94e0eb9e2a123469283d7d715c647ee85e584d
Charlie Porth <charlieporth1@gmail.com>
2024-11-24 20:40:10 -0600

    Added key

--- helper/scripts/install.sh
+++ helper/scripts/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chmod +x *.sh
-wget -O deadsnakes-ubuntu-ppa.gpg https://keyserver.ubuntu.com/pks/pool/archive/key/F23C5A6CF475977595C89F51BA6932366A755776
-sudo gpg --keyserver hkps://keyserver.ubuntu.com:443 --import deadsnakes-ubuntu-ppa.gpg
+wget -O /usr/share/keyrings/deadsnakes-ubuntu-ppa.gpg https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu/dists/jammy/Release.gpg
 cp -rfv ../config/* /etc/
 ./python-install.sh
 ./python3-replace.sh
