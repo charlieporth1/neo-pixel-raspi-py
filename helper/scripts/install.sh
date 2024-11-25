8ba8345b674342876ee4d998044474e2108a7419
Charlie Porth <charlieporth1@gmail.com>
2024-11-24 20:56:23 -0600

    Apt key fix

--- helper/scripts/install.sh
+++ helper/scripts/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 chmod +x *.sh
-wget -O /usr/share/keyrings/deadsnakes-ubuntu-ppa.gpg https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu/dists/jammy/Release.gpg
+curl -fsSL https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu/dists/jammy/Release.gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/deadsnakes-ubuntu-ppa.gpg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BA6932366A755776 
 cp -rfv ../config/* /etc/
 ./python-install.sh
 ./python3-replace.sh
