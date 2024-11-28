359b820a54343e2cd35d0360dc48bbca5b4ccada
Charlie Porth <charlieporth1@gmail.com>
2024-11-28 13:10:07 -0600

    Updated install script

--- install.sh
+++ install.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 echo $PWD
 cd $PWD
-sudo apt install -y python3-dev python3-pip scons cmake swig git
-sudo apt install -y build-essential python-dev git scons swig libzbar-dev libzbar0
-sudo apt install -y python python-pip libffi-dev python-backports.ssl-match-hostname
-sudo apt install -y python3-pip
-sudo apt install -y python3-full python3 python3-pip3
-sudo apt install libcblas-dev
-sudo apt install libhdf5-dev
-sudo apt install libhdf5-serial-dev
-sudo apt install libatlas-base-dev
-sudo apt install libjasper-dev 
-sudo apt install libqtgui4 
-sudo apt install libqt4-test
-sudo apt install libraspberrypi-bin
-sudo apt install libatlas-base-dev
+apt install -y scons cmake swig git
+apt install -y build-essential
+apt install -y git
 
-sudo pip3 install vcgencmd
-sudo pip install rpi_ws281x
-sudo pip3 install rpi_ws281x --break-system-packages
+apt install -y python
+apt install -y python-pip
+apt install -y python-dev
+apt install -y python-backports.ssl-match-hostname
+
+apt install -y python3
+apt install -y python3-full
+apt install -y python3-dev
+apt install -y python3-pip3
+
+apt install -y python-is-python3
+apt install -y 2to3
+apt install -y python-dev-is-python3
+
+apt install -y libffi-dev
+apt install -y libzbar-dev
+apt install -y libzbar0
+apt install -y libcblas-dev
+apt install -y libhdf5-dev
+apt install -y libhdf5-serial-dev
+apt install -y libatlas-base-dev
+apt install -y libjasper-dev
+apt install -y libqtgui4
+apt install -y libqt4-test
+apt install -y libraspberrypi-bin
+apt install -y libatlas-base-dev
+
+pip3 install vcgencmd
+pip install rpi_ws281x
+pip3 install rpi_ws281x --break-system-packages
 
 git submodule init
 git submodule update
 
 cd rpi_ws281x/python
-sudo python3 setup.py  install
+python3 setup.py install
+python3 ez_setup.py
+
 cd -
+
 #cd ~/ && git clone https://github.com/jgarff/rpi_ws281x
 #cd rpi_ws281x
 #mkdir build
 #cd build
 #cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON ..
-#sudo make install
+#make install
 
 echo "Installing"
-sudo chmod 777 *.py *.sh
+chmod 777 *.py *.sh
 ln -s $PWD/*.py /usr/local/bin
 echo "Done"
